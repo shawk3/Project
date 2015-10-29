2a8798347f170dccc4b3cabc2f8f2279b17031e2
Kyle <sha11005@byui.edu>
2015-10-28 18:21:59 -0600

    first buttons on Main Page, clickButton functions made but empty

--- app/src/main/java/edu/byui/cs246/project/MainActivity.java
+++ app/src/main/java/edu/byui/cs246/project/MainActivity.java
@@ -2,6 +2,10 @@ package edu.byui.cs246.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static java.lang.System.exit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void clickLoad(View v){
+        //remain on page, enable options
+        showOtherOptions();
+    }
 
+    public void clickNew(View v){
+        //go to a new page
+        showOtherOptions();
+    }
+
+    public void clickQuit(View v){
+        exit(1);
+    }
+
+    private void showOtherOptions(){
+        Button a = (Button) findViewById(R.id.demographics_b);
+        a.setVisibility(View.VISIBLE);
+        Button b = (Button) findViewById(R.id.questions_b);
+        b.setVisibility(View.VISIBLE);
+        Button c = (Button) findViewById(R.id.analysis_b);
+        c.setVisibility(View.VISIBLE);
+    }
 }

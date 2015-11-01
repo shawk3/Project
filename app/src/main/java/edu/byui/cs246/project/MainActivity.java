908c26241e259936eebde3dbced6e2137eb1c9fe
Kyle <sha11005@byui.edu>
2015-10-31 20:00:16 -0600

    Test the new Database Class in the MainActivity

--- app/src/main/java/edu/byui/cs246/project/MainActivity.java
+++ app/src/main/java/edu/byui/cs246/project/MainActivity.java
@@ -1,9 +1,11 @@
 package edu.byui.cs246.project;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import static java.lang.System.exit;
 
@@ -18,11 +20,39 @@ public class MainActivity extends AppCompatActivity {
     public void clickLoad(View v){
         //remain on page, enable options
         showOtherOptions();
+
+        //Testing code
+        DataBase db = new DataBase(this);
+        db.open();
+
+        Cursor c = db.getAllRows();
+
+        if(c.moveToFirst()){
+            int id = c.getInt(0);
+            String text = c.getString(1);
+            String answer = c.getString(2);
+
+            TextView textBox = (TextView) findViewById(R.id.textView);
+            textBox.setText("id: " + id + ";  Question: " + text + "\n Answer: " + answer);
+        }
+
+        c.close();
+        db.close();
+
+
     }
 
     public void clickNew(View v){
         //go to a new page
         showOtherOptions();
+
+        //Testing code
+        DataBase db = new DataBase(this);
+        db.open();
+
+        db.insertRow("My first Question", "U");
+
+        db.close();
     }
 
     public void clickQuit(View v){

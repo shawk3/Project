3a1902152e74166a838ba1bea65016a8d7e05e1e
Kyle <sha11005@byui.edu>
2015-12-01 21:24:40 -0700

    Created a table on the load page

--- app/src/main/java/edu/byui/cs246/project/loadScreen.java
+++ app/src/main/java/edu/byui/cs246/project/loadScreen.java
@@ -1,13 +1,57 @@
 package edu.byui.cs246.project;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class loadScreen extends AppCompatActivity {
+    DataBase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_screen);
+
+        db = new DataBase(this);
+        db.open();
+
+        Cursor c = db.getAllRows(db.SESSION_TABLE);
+        TableLayout table = (TableLayout) findViewById(R.id.table);
+
+        if(c.getCount() == 0){
+            TableRow tR = new TableRow(this);
+
+            TextView text = new TextView(this);
+            text.setText("There are no previous files");
+
+            tR.addView(text);
+
+            table.addView(tR);
+            //display text "no previous files"
+        }
+        else{
+            c.moveToFirst();
+            do{
+                TableRow tR = new TableRow(this);
+                tR.setPadding(5, 5, 5, 5);
+
+                TextView text = new TextView(this);
+                text.setText(c.getString(db.COL_SESSION_NAME));
+
+                CheckBox box = new CheckBox(this);
+
+                tR.addView(text);
+                tR.addView(box);
+    
+                table.addView(tR);
+            } while(c.moveToNext());
+        }
+
     }
 }

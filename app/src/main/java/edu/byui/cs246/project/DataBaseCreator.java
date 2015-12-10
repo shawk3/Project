7ba2f4f0121d8254f3c52c19d68763d10f259d41
Kyle <sha11005@byui.edu>
2015-12-09 20:03:19 -0700

    Delete some unused code in the DataBaseCreator class

--- app/src/main/java/edu/byui/cs246/project/DataBaseCreator.java
+++ app/src/main/java/edu/byui/cs246/project/DataBaseCreator.java
@@ -187,7 +187,7 @@ public class DataBaseCreator {
         createSectors();
         createSubSectors();
         createSectorSubSectorMap();
-        createSessions();
+        //createSessions();
         dataBase.close();
 
 

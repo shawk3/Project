c9a0069f5cc130e1214f31b6418644d17cc340a4
Jason Newquist <jasonewquist@ymail.com>
2015-12-10 23:09:34 -0700

    added JavaDocs for DemographicsActivity, DemographicsData, and SectorAdapter

--- app/src/main/java/edu/byui/cs246/project/DemographicsActivity.java
+++ app/src/main/java/edu/byui/cs246/project/DemographicsActivity.java
@@ -1,17 +1,10 @@
 package edu.byui.cs246.project;
 
-import android.app.Fragment;
-import android.app.ListActivity;
-import android.content.ClipData;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -26,28 +19,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import edu.byui.cs246.project.SectorAdapter;
+
 /**
+ * Class for selecting a sector and sub-sector
+ *
+ * the sector page allows the user to choose a sector and
+ * sub-sector for their given profile
+ *
  * @author Jason
  * @since 2015-11
  */
-import edu.byui.cs246.project.SectorAdapter;
-
 public class DemographicsActivity extends AppCompatActivity {
 
+    /** The database */
     DataBase db;
+    /** database helper */
+    DemographicsData data;
+    /** storing sessions */
     SharedPreferences settings;
+    /** profile id */
     int sessionID;
-    /*String[] sectors = {"Chemical", "Commercial Facilities", "Communications",
-            "Critical Manufacturing", "Dams", "Defense Industrial Base", "Emergency Services",
-            "Energy", "Financial Services", "Food and Agriculture", "Government Facilities",
-            "Healthcare and Public Health", "Information Technology", "Nuclear Reactors, Materials, and Waste",
-            "Sector-Specific Agencies", "Transportation Systems", "Water and Wastewater Systems"};*/
-
-
-    //ArrayAdapter<String> adapter;
+    /** HashMap of Sectors with associated sub-sectors*/
     HashMap<String, List<String>> Sector_And_Subs;
+    /** list of sectors */
     List<String> Sector_List;
+    /** the list that sectors and sub-sectors are put into */
     ExpandableListView sectorList;
+    /** adapter for ExpandableListView */
     SectorAdapter sectorAdapter;
 
     @Override
@@ -58,58 +57,43 @@ public class DemographicsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Sector");
 
+        /** opening the database */
         db = new DataBase(this);
         db.open();
 
+        /** creating the session */
         settings = getSharedPreferences("settingsFile", 0);
         sessionID = settings.getInt("Session", 0);
 
+        /** retrieving data from DemographicsData */
+        data = new DemographicsData(db, sessionID);
+
+        /** declaration of certain class variables */
         sectorList = (ExpandableListView) findViewById(R.id.sectorList);
-        Sector_And_Subs = getInfo();
+        Sector_And_Subs = data.createMapList();
         Sector_List = new ArrayList<String>(Sector_And_Subs.keySet());
         sectorAdapter = new SectorAdapter(this, Sector_And_Subs, Sector_List);
-        sectorList.setAdapter(sectorAdapter);
 
-        sectorList.setOnItemClickListener(new ListView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                view.setSelected(true);
-            }
-        });
+        /** setting adapter to its default state */
+        sectorAdapter.setDefault(data.getSectorName(), data.getSubSectorName());
+        sectorList.setAdapter(sectorAdapter);
+        int g = sectorAdapter.getDefaultGroup();
+        if(g >= 0)
+            sectorList.expandGroup(g);
+
+        /** what happens when a sub-sector is clicked */
         sectorList.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
             public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
                 v.setSelected(true);
                 Toast.makeText(getBaseContext(), Sector_And_Subs.get(Sector_List.get(groupPosition)).get(childPosition) + " was clicked", Toast.LENGTH_LONG).show();
-                //startActivity(new Intent(getApplicationContext(), QuestionActivity.class));
+                String sid = sectorAdapter.getGroup(groupPosition).toString();
+                String subID = sectorAdapter.getChild(groupPosition, childPosition).toString();
+                data.updateSession(sid, subID);
+
                 return true;
             }
         });
-
-      /*  sectorList.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
-            @Override
-            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-                if(Sector_List.get(groupPosition).isEmpty()){
-                    startActivity(new Intent(getApplicationContext(), QuestionActivity.class));
-                    return true;
-                }
-                return true;
-            }
-        });
-*/
-        /*sectorList = (ExpandableListView) findViewById(R.id.sectorList);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sectors);
-        sectorList.setAdapter(adapter);
-        sectorList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                view.setSelected(true);
-
-
-            }
-        });
-        */
-
     }
 
     /**
@@ -152,56 +136,5 @@ public class DemographicsActivity extends AppCompatActivity {
                 return false;
         }
         return true;
-    }
-
-    //Example function
-    public void onClick(String text){
-        List<String> subSectors= new ArrayList<String>();
-
-        Cursor SidCursor = db.getRow(db.SECTOR_TABLE, text);
-        int Sid = SidCursor.getInt(db.COL_SECTOR_ID);
-        Cursor c = db.getSubSectors(Sid);
-        do{
-            String j = c.getString(db.COL_SUB_SECTOR_ID);
-            subSectors.add(j);
-        }while(c.moveToNext());
-
-
-
-    }
-
-    public HashMap<String, List<String>> getInfo() {
-        HashMap<String, List<String>> Sectors_And_Subs = new HashMap<String, List<String>>();
-
-        Cursor sect = db.getAllRows(db.SECTOR_TABLE);
-        sect.moveToFirst();
-
-        do{
-            String sectorName = sect.getString(db.COL_SECTOR);
-            List<String> subList = new ArrayList<String>();
-
-            if(!sectorName.equals("Default")) {
-                int Sid = sect.getInt(db.COL_ROWID);
-                Cursor subSIDs = db.getSubSectors(Sid);
-
-
-                if (subSIDs != null) {
-                    subSIDs.moveToFirst();
-                    do {
-                        int subId = subSIDs.getInt(db.COL_SUB_SECTOR_ID);
-                        Cursor sub = db.getRow(db.SUB_SECTOR_TABLE, subId);
-                        String subSectorName = sub.getString(db.COL_SUB_SECTOR);
-                        if(subSectorName.equals("Default"))
-                            subSectorName = "No Sub-Sectors listed for this Sector";
-                        subList.add(subSectorName);
-                    } while (subSIDs.moveToNext());
-                }
-
-
-                Sectors_And_Subs.put(sectorName, subList);
-            }
-        } while(sect.moveToNext());
-
-        return Sectors_And_Subs;
     }
 }
--- app/src/main/java/edu/byui/cs246/project/SectorAdapter.java
+++ app/src/main/java/edu/byui/cs246/project/SectorAdapter.java
@@ -17,17 +17,36 @@ import java.util.List;
 import edu.byui.cs246.project.R;
 
 /**
- * Created by Jason on 12/9/2015.
+ * adapter for ExpandableListView
+ *
+ * this class allows users to interact with the ExpandableLIstView found
+ * in the Demographics Activity
+ *
+ * @author Jason
+ * @since 2015-12
  */
 public class SectorAdapter extends BaseExpandableListAdapter {
 
+    /** context variable */
     private Context ctx;
+    /** HashMap for sectors and sub-sectors*/
     private HashMap<String, List<String>> Sectors;
+    /** list of sectors */
     private List<String> Sector_List;
+    /** string for sector with no sub-sectors */
     String defaultSector;
+    /** no sub-sector group */
     int defaultGroup;
+    /** string for no sub-sector */
     String defaultSubSect;
 
+    /**
+     * Non-default constructor for adapter
+     *
+     * @param ctx
+     * @param Sectors
+     * @param Sector_List
+     */
     public SectorAdapter(Context ctx, HashMap<String, List<String>> Sectors, List<String> Sector_List){
         this.ctx = ctx;
         this.Sectors = Sectors;
@@ -81,6 +100,15 @@ public class SectorAdapter extends BaseExpandableListAdapter {
         return false;
     }
 
+    /**
+     * return sector view
+     *
+     * @param parent
+     * @param isExpanded
+     * @param convertView
+     * @param parentview
+     * @return
+     */
     @Override
     public View getGroupView(int parent, boolean isExpanded, View convertView, ViewGroup parentview) {
         String group_title = (String) getGroup(parent);
@@ -96,6 +124,16 @@ public class SectorAdapter extends BaseExpandableListAdapter {
         return convertView;
     }
 
+    /**
+     * return sub-sector view
+     *
+     * @param parent
+     * @param child
+     * @param lastChild
+     * @param convertView
+     * @param parentview
+     * @return
+     */
     @Override
     public View getChildView(int parent, int child, boolean lastChild, View convertView, ViewGroup parentview) {
         String child_title = (String) getChild(parent, child);
@@ -114,12 +152,27 @@ public class SectorAdapter extends BaseExpandableListAdapter {
         return convertView;
     }
 
+    /**
+     * child is selectable
+     *
+     * @param groupPosition
+     * @param childPosition
+     * @return
+     */
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
 
         return true;
     }
 
+    /**
+     * setter
+     *
+     * sets default group
+     *
+     * @param sID
+     * @param subID
+     */
     public void setDefault(String sID, String subID){
         defaultSector = sID;
         defaultSubSect = subID;
@@ -127,5 +180,12 @@ public class SectorAdapter extends BaseExpandableListAdapter {
         setDefaultGroup();
     }
 
-    public int getDefaultGroup(){return defaultGroup; };
+    /**
+     * getter
+     *
+     * return default group
+     *
+     * @return
+     */
+    public int getDefaultGroup(){return defaultGroup; }
 }
